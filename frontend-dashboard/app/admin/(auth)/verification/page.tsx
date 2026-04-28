82c00610c1ce56a672a129904b264481396c082b
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2026-04-28 17:12:10 +0600

    feat: verification/page.tsx

--- frontend-dashboard/app/admin/(auth)/verification/page.tsx
+++ frontend-dashboard/app/admin/(auth)/verification/page.tsx
@@ -93,7 +93,6 @@ export default function Verification() {
                         {otp.map((digit, index) => (
                             <input
                                 key={index}
-                                {/* ref={(el) => (inputRefs.current[index] = el)} */}
                                 ref={(el) => {inputRefs.current[index] = el;}}
                                 type="text"
                                 inputMode="numeric"
